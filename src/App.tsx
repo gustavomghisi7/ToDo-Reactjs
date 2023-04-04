b36c2b93c2ff95fad700d7c06d879c12c1dc9d46
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2023-04-04 14:49:18 -0300

    challenge completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { TaskList } from './components/TaskList'
 import { Header } from "./components/Header";
-import './styles/global.scss'
 
+import './styles/global.scss'
 
 export function App() {
   return (
